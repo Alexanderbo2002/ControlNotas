d8106ed956bea59d9d8c9d0fdce97908069a3c4d
Alexanderbo2002 <ericbarillas9@gmail.com>
2024-08-30 23:05:12 -0600

    Create ControlNotas.java

---
+++ logica/ControlNotas.java
@@ -0,0 +1,15 @@
+package logica; 
+// Importación del paquete para la pantalla
+import igu.Pantalla;
+
+public class ControlNotas {
+
+    public static void main(String[] args) {
+        // Crear una instancia de Pantalla y hacerla visible
+        Pantalla panta = new Pantalla(); 
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+    }   
+}
